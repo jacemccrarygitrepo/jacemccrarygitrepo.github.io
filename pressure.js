b73c631975d76d361e98e05072f6682d94301e39
jacemccrarygitrepo <jacemccrarygitrepo@mail.com>
2025-06-05 13:29:36 -0500

    Add files via upload

---
+++ pressure.js
@@ -0,0 +1,11 @@
+function updateRange(val) {
+    console.log("val = ",val);
+    val3 = val + val;
+    console.log("val3 = ",val3);
+    document.getElementById("output").value=val;
+}
+
+function updateText(val2) { /* Update text value function */
+    console.log("val2 = ",val2);
+    document.getElementById("range_1").value = val2;
+}
